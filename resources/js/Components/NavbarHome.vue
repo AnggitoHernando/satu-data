277c3ffd4b1e7ab4ed7105303a6ddc16b8ab8d95
unknown <anggito.hernando@gmail.com>
2025-11-03 12:37:00 +0700

    Perbaikan Navbar Jika Login dan Tidak Login

--- resources/js/Components/NavbarHome.vue
+++ resources/js/Components/NavbarHome.vue
@@ -1,13 +1,17 @@
 <script setup>
 import NavLink from "./NavLink.vue";
 import PrimaryButtonAdmin from "./PrimaryButtonAdmin.vue";
-import { Link } from "@inertiajs/vue3";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import { Link, usePage } from "@inertiajs/vue3";
 import { ref } from "vue";
 const isOpen = ref(false);
 import { Menu, X } from "lucide-vue-next";
 function toggleMenu() {
     isOpen.value = !isOpen.value;
 }
+const page = usePage();
+const user = page.props?.auth?.user?.name;
 </script>
 <template>
     <div>
@@ -42,13 +46,56 @@ function toggleMenu() {
                             >
                                 Portal Data
                             </NavLink>
+                            <NavLink v-if="user" :href="route('dashboard')">
+                                Dashboard Admin
+                            </NavLink>
 
                             <div class="lg:ml-8 md:ml-4">
-                                <Link :href="route('login')">
+                                <Link :href="route('login')" v-if="!user">
                                     <PrimaryButtonAdmin>
                                         Login</PrimaryButtonAdmin
                                     >
                                 </Link>
+                                <Dropdown align="right" width="48" v-else>
+                                    <template #trigger>
+                                        <span class="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-green-800 hover:text-white-400 focus:outline-none transition ease-in-out duration-150"
+                                            >
+                                                {{ user }}
+
+                                                <svg
+                                                    class="-me-0.5 ms-2 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clip-rule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </template>
+
+                                    <template #content>
+                                        <!-- <DropdownLink
+                                            :href="route('profile.edit')"
+                                        >
+                                            Profile
+                                        </DropdownLink> -->
+                                        <DropdownLink
+                                            :href="route('logout')"
+                                            method="post"
+                                            as="button"
+                                        >
+                                            Log Out
+                                        </DropdownLink>
+                                    </template>
+                                </Dropdown>
                             </div>
                         </div>
 
